25cfe16c38187508ce53c4fa570423482d278636
Will Dixon <wdixon23192@gmail.com>
2015-08-04 23:16:34 -0400

    worked on accept options, fixed inheritance issues

--- wwd9d.py
+++ wwd9d.py
@@ -5,19 +5,22 @@ from random import random, shuffle
 class wwd9d(BaseNegotiator):
 
     def __init__(self):
-        super.__init__()
-        turnCounter = 0
-        utility = 0
-        results = 0
+        BaseNegotiator.__init__(self)
+        self.turn_counter = 0
+        self.curr_utility = 0
+        self.results = 0
+        self.is_first = False
 
 
     def make_offer(self, offer):
+        print "using overloaded method"
         # is it not the first turn
         if offer:
             # calculate a bunch of things
-            self.turnCounter += 1
+            self.turn_counter += 1
         else:
-            turnCounter = 0
+            self.turn_counter = 1
+            self.is_first = True
 
         # is it the first
 
@@ -27,29 +30,59 @@ class wwd9d(BaseNegotiator):
         print self.preferences
 
         # if it is our best, take it
-        if self.compare(self, self.preferences, self.order):
+        if self.compare(self.preferences, self.offer):
+            print "best offer"
             self.offer = offer
             return
 
         # last turn price protection
-        if (self.iter_limit-1) == turnCounter:
+        # we expect that on the last turn the opponent will attempt to send a bad deal
+        # this attempts to dodge that, accepting a slight penalty to avoid this
+        if (self.iter_limit-1) == self.turn_counter and self.is_first:
+            print "last turn dodge"
             #make sure offer is worthwhile for us
+            #penalty is - the length of the ordering
+            if self.utility > -len(offer):
+                #take the last offer
+                self.offer = offer
+                return
+
+        # last turn acceptance, just in case
+        if self.iter_limit == self.turn_counter:
+            print "last turn acceptance"
+            print "len offer"+  len(offer)
+            if self.utility > -len(offer):
+                #take the last offer
+                self.offer = offer
+                return
+        #temp last turn acceptance
+        if self.iter_limit == self.turn_counter:
+            print "edge case hit"
+            self.offer = offer
+            return
+
         # general acceptance
 
         # reject offer
 
-    def receive_utility(self, utility):
-        self.utility = utility
+
+    def receive_utility(self, curr_utility):
+        self.curr_utility = curr_utility
 
     def receive_results(self, results):
         self.results = results
 
     def compare(self, list_a, list_b):
         is_same = True
-        for elem_a in list_a:
-            for elem_b in list_b:
-                if elem_a == elem_b:
-                    isSame = False
+        print len(list_b)
+        print len(list_a)
+        if len(list_b) == len(list_a):
+            for elem_a in list_a:
+                for elem_b in list_b:
+                    if elem_a != elem_b:
+                        is_same = False
+        else:
+            is_same = False
         return is_same
 
         #  if random() < 0.05 and offer:
